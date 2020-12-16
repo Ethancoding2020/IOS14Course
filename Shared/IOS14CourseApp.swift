7aa6c88a75cacf3804aeb0baf3f07a6dcb4982ef
Ethan L <coding.ethan@gmail.com>
2020-12-16 12:02:51 -0500

    Initial Commit

---
+++ Shared/IOS14CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS14CourseApp.swift
+//  Shared
+//
+//  Created by Ethan on 2020-12-16.
+//
+
+import SwiftUI
+
+@main
+struct IOS14CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
