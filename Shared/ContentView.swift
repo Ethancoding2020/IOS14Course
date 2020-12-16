f2feee1a63a8db84faf04b0b7393472a5714f235
Ethan L <coding.ethan@gmail.com>
2020-12-16 13:23:49 -0500

    Video P2 14min

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Image("Illustration 1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("SwiftUI for iOS")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+            Text("20 Sections")
+                .font(.footnote).foregroundColor(Color.white)
+        }
+        .padding(.all)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
     }
 }
 
